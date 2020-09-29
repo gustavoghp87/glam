beaadd208021666dda8f9b46f9cb2256dc5e1a51
gustavoghp87 <ghp.2120@gmail.com>
2020-09-29 13:22:49 -0300

    build again

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,11 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-<<<<<<< HEAD
   "/precache-manifest.e3eef28e062eff277866572e95328b0e.js"
-=======
-  "/precache-manifest.be94864aa50f4f3b90f89530fe55e643.js"
->>>>>>> tmp
 );
 
 self.addEventListener('message', (event) => {
