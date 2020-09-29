051d8b0134bf787ca9a3774f877dcb9907c81781
gustavoghp87 <ghp.2120@gmail.com>
2020-09-29 13:40:31 -0300

    package server

---
+++ client/build/precache-manifest.e3eef28e062eff277866572e95328b0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a6ae4586ac9d381ee415addb2fbf53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e23b4a5be9f7cba1f439",
+    "url": "/static/css/2.433f6543.chunk.css"
+  },
+  {
+    "revision": "bf8102a78dcff69be2d3",
+    "url": "/static/css/main.53a3f8c4.chunk.css"
+  },
+  {
+    "revision": "e23b4a5be9f7cba1f439",
+    "url": "/static/js/2.a6727ab0.chunk.js"
+  },
+  {
+    "revision": "1cefadf8f6ea1078da73db3047ca72e2",
+    "url": "/static/js/2.a6727ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf8102a78dcff69be2d3",
+    "url": "/static/js/main.06e7eba3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
